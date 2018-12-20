d132ef57e749894a867bab48ceb50648f6ac193d
FJinn <0123433@kdu-online.com>
2018-12-20 17:08:25 +0800

    1. Added Item, Skill, GirlController scripts. ---> 1a. Item and Skill scripts will probably change name. GirlController is Working In Progress. Need to update class diagram first, to see a clearer picure. 2. Deleted terrain folder in RainMaker, should have no problem without it.

---
+++ Assets/Scripts/Girl/GirlController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GirlController : MonoBehaviour
+{
+    enum EmotionType
+    {
+        Happy,
+        Sad,
+        Angry,
+        Scare,
+        Neutral
+    }
+
+    // my attributes
+    [SerializeField] float myHP;
+    [SerializeField] float myEQ;
+    [SerializeField] int myKnowledgePoint;
+
+    // my emotion status
+    [SerializeField] float myEmotionLevel;
+    [SerializeField] EmotionType myEmotion;
+
+    // my current skill
+    [SerializeField] static List<Skill> skillList;
+
+    // my current item
+    [SerializeField] static List<Item> itemList;
+
+    // in battle variable
+    // Ally
+    [SerializeField] Ally selectedAlly;
+
+    // Action
+    Action myAction;
+
+
+}
